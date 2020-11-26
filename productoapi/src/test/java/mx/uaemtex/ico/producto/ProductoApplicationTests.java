6054fcd392e86551e5b91c058fab78a7bb2a55b3
tmsanchez <tmsanchez@gmail.com>
2020-11-26 03:23:04 -0600

    Creacion del proyecto con Spring Boot

---
+++ productoapi/src/test/java/mx/uaemtex/ico/producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uaemtex.ico.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
